4f36ff55208f10fa545b95374ded698252d84c36
Nguyễn Hữu Huy <93850157+HuuHuy227@users.noreply.github.com>
2022-11-28 13:10:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from pydantic import BaseModel
-import torch
-import torch.nn as nn
-from torch import Tensor
-from torch.nn import Transformer
-from torch.nn.utils.rnn import pad_sequence
-from torchtext.data.utils import get_tokenizer
-import math
-import pickle
-from typing import List
-import warnings
-warnings.filterwarnings('ignore')
-
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # Check whether your device run on gpu or cpu
-# Define special symbols and indices                                    
-UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+from torch_utils import translate #Import predicting func
 
 app = FastAPI(debug = True)
 
@@ -33,185 +19,6 @@ app.add_middleware(  # Enable CORS
 class body_request(BaseModel):
     encSeq: str
 
-# helper Module that adds positional encoding to the token embedding to introduce a notion of word order.
-class PositionalEncoding(nn.Module):
-    def __init__(self,
-                 emb_size: int,
-                 dropout: float = 0.1,
-                 maxlen: int = 5000):
-        super(PositionalEncoding, self).__init__()
-        
-        den = torch.exp(- torch.arange(0, emb_size, 2)* math.log(10000) / emb_size)
-        pos = torch.arange(0, maxlen).reshape(maxlen, 1)
-        pos_embedding = torch.zeros((maxlen, emb_size))
-        pos_embedding[:, 0::2] = torch.sin(pos * den)
-        pos_embedding[:, 1::2] = torch.cos(pos * den)
-        pos_embedding = pos_embedding.unsqueeze(-2)
-
-        self.dropout = nn.Dropout(dropout)
-        self.register_buffer('pos_embedding', pos_embedding)
-
-    def forward(self, token_embedding: Tensor):
-        return self.dropout(token_embedding + self.pos_embedding[:token_embedding.size(0), :])
-
-# helper Module to convert tensor of input indices into corresponding tensor of token embeddings
-class TokenEmbedding(nn.Module):
-    def __init__(self, vocab_size: int, emb_size):
-        super(TokenEmbedding, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, emb_size) 
-        self.emb_size = emb_size
-
-    def forward(self, tokens: Tensor):
-        return self.embedding(tokens.long()) * math.sqrt(self.emb_size)
-
-# Seq2Seq Network
-class Seq2SeqTransformer(nn.Module):
-    def __init__(self,
-                 num_encoder_layers: int,
-                 num_decoder_layers: int,
-                 emb_size: int,
-                 nhead: int,
-                 src_vocab_size: int,
-                 tgt_vocab_size: int,
-                 dim_feedforward: int = 512,
-                 dropout: float = 0.1):
-        super(Seq2SeqTransformer, self).__init__()
-        self.transformer = Transformer(d_model=emb_size,
-                                       nhead=nhead,
-                                       num_encoder_layers=num_encoder_layers,
-                                       num_decoder_layers=num_decoder_layers,
-                                       dim_feedforward=dim_feedforward,
-                                       dropout=dropout)
-        self.generator = nn.Linear(emb_size, tgt_vocab_size)
-        self.src_tok_emb = TokenEmbedding(src_vocab_size, emb_size)
-        self.tgt_tok_emb = TokenEmbedding(tgt_vocab_size, emb_size)
-        self.positional_encoding = PositionalEncoding(
-            emb_size, dropout=dropout)
-
-    def forward(self,
-                src: Tensor,
-                trg: Tensor,
-                src_mask: Tensor,
-                tgt_mask: Tensor,
-                src_padding_mask: Tensor,
-                tgt_padding_mask: Tensor,
-                memory_key_padding_mask: Tensor):
-        src_emb = self.positional_encoding(self.src_tok_emb(src))
-        tgt_emb = self.positional_encoding(self.tgt_tok_emb(trg))
-        outs = self.transformer(src_emb, tgt_emb, src_mask, tgt_mask, None,
-                                src_padding_mask, tgt_padding_mask, memory_key_padding_mask)
-        return self.generator(outs)
-   
-    def encode(self, src: Tensor, src_mask: Tensor):
-        return self.transformer.encoder(self.positional_encoding(
-                            self.src_tok_emb(src)), src_mask)
-
-    def decode(self, tgt: Tensor, memory: Tensor, tgt_mask: Tensor):
-        return self.transformer.decoder(self.positional_encoding(
-                          self.tgt_tok_emb(tgt)), memory,
-                          tgt_mask)
-
-def generate_square_subsequent_mask(sz):
-    mask = (torch.triu(torch.ones((sz, sz), device=DEVICE)) == 1).transpose(0, 1)
-    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-    return mask
-
-
-def create_mask(src, tgt):
-    src_seq_len = src.shape[0]
-    tgt_seq_len = tgt.shape[0]
-
-    tgt_mask = generate_square_subsequent_mask(tgt_seq_len)
-    src_mask = torch.zeros((src_seq_len, src_seq_len),device=DEVICE).type(torch.bool)
-
-    src_padding_mask = (src == PAD_IDX).transpose(0, 1)
-    tgt_padding_mask = (tgt == PAD_IDX).transpose(0, 1)
-    return src_mask, tgt_mask, src_padding_mask, tgt_padding_mask
-
-EMB_SIZE = 512
-NHEAD = 8 # embed_dim must be divisible by num_heads
-FFN_HID_DIM = 512
-NUM_ENCODER_LAYERS = 4
-NUM_DECODER_LAYERS = 4
-DROP_OUT = 0.1
-
-def read_vocab(path):
-    #import pickle
-    pkl_file = open(path, 'rb')
-    vocab = pickle.load(pkl_file)
-    pkl_file.close()
-    return vocab
-
-def load_model_and_vocab():
-    vocab_src_path = './Pretrained_Weights/vocab_src.pkl'
-    vocab_tgt_path = './Pretrained_Weights/vocab_tgt.pkl'
-    weights_path = './Pretrained_Weights/viEn_transformer.pth'
-    
-    vocab_src = read_vocab(vocab_src_path)
-    vocab_tgt = read_vocab(vocab_tgt_path)
-    SRC_VOCAB_SIZE = len(vocab_src)
-    TGT_VOCAB_SIZE = len(vocab_tgt)
-    model = Seq2SeqTransformer(NUM_ENCODER_LAYERS, NUM_DECODER_LAYERS, EMB_SIZE,
-                                 NHEAD, SRC_VOCAB_SIZE, TGT_VOCAB_SIZE, FFN_HID_DIM,DROP_OUT)
-    model.load_state_dict(torch.load(weights_path)) # Using torch.load(weights_path,map_location = DEVICE) if running error on cpu
-    model.eval()
-    return  model, vocab_src, vocab_tgt
-
-transformer, vocab_src, vocab_tgt = load_model_and_vocab()
-transformer = transformer.to(DEVICE)
-
-# helper function to club together sequential operations
-def sequential_transforms(*transforms):
-    def func(txt_input):
-        for transform in transforms:
-            txt_input = transform(txt_input)
-        return txt_input
-    return func
-
-# function to add BOS/EOS and create tensor for input sequence indices
-def tensor_transform(token_ids: List[int]):
-    return torch.cat((torch.tensor([BOS_IDX]),
-                      torch.tensor(token_ids),
-                      torch.tensor([EOS_IDX])))
-
-token_transform = get_tokenizer('basic_english')
-text_transform = sequential_transforms(token_transform, #Tokenization
-                                        vocab_src, #Numericalization
-                                        tensor_transform) # Add BOS/EOS and create tensor
-
-# function to generate output sequence using greedy algorithm
-def greedy_decode(model, src, src_mask, max_len, start_symbol):
-    src = src.to(DEVICE)
-    src_mask = src_mask.to(DEVICE)
-
-    memory = model.encode(src, src_mask)
-    ys = torch.ones(1, 1).fill_(start_symbol).type(torch.long).to(DEVICE)
-    for i in range(max_len-1):
-        memory = memory.to(DEVICE)
-        tgt_mask = (generate_square_subsequent_mask(ys.size(0))
-                    .type(torch.bool)).to(DEVICE)
-        out = model.decode(ys, memory, tgt_mask)
-        out = out.transpose(0, 1)
-        prob = model.generator(out[:, -1])
-        _, next_word = torch.max(prob, dim=1)
-        next_word = next_word.item()
-
-        ys = torch.cat([ys,
-                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=0)
-        if next_word == EOS_IDX:
-            break
-    return ys
-
-
-# actual function to translate input sentence into target language
-def translate(model: torch.nn.Module, src_sentence: str):
-    src = text_transform(src_sentence).view(-1, 1)
-    num_tokens = src.shape[0]
-    src_mask = (torch.zeros(num_tokens, num_tokens)).type(torch.bool)
-    tgt_tokens = greedy_decode(
-        model,  src, src_mask, max_len=num_tokens + 5, start_symbol=BOS_IDX).flatten()
-    return " ".join(vocab_tgt.lookup_tokens(list(tgt_tokens.cpu().numpy()))).replace("<bos>", "").replace("<eos>", "")                                       
-
 
 @app.get("/")
 async def home():
@@ -220,7 +27,7 @@ async def home():
 @app.post("/predict")
 async def predict(item: body_request):
     # Make prediction
-    pred = translate(transformer, item.encSeq)
+    pred = translate(item.encSeq)
     return {"code" : 0, "result" : pred}
 
 if __name__ == '__main__':
